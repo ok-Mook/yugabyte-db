27d2f073c680fd2ffef9f5a6f871018aa9ab2e27
Dylan <160562249+ok-Mook@users.noreply.github.com>
2025-05-30 18:07:46 +0000

    Add YSQL enabled check to UpgradeYsql and corresponding test

--- src/yb/tserver/tablet_service.cc
+++ src/yb/tserver/tablet_service.cc
@@ -2300,6 +2300,12 @@ void TabletServiceAdminImpl::UpgradeYsql(
     rpc::RpcContext context) {
   LOG(INFO) << "Starting YSQL upgrade";
 
+  if(!FLAGS_enable_ysql) {
+    LOG(INFO) << "YSQL upgrade failed: YSQL is not enabled.";
+    SetupErrorAndRespond(resp->mutable_error(), status, &context);
+    return;
+  }
+  
   pgwrapper::YsqlUpgradeHelper upgrade_helper(server_->pgsql_proxy_bind_address(),
                                               server_->GetSharedMemoryPostgresAuthKey(),
                                               FLAGS_heartbeat_interval_ms,
